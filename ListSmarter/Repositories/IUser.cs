ce6a3229c9d4bc8fd40767b790ed679ef1d7052b
auhiriwe <u.audace@gmail.com>
2023-02-21 08:47:46 +0000

    Add user controller, services and repository

---
+++ ListSmarter/Repositories/IUser.cs
@@ -0,0 +1,18 @@
+﻿using ListSmarter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListSmarter.Repositories
+{
+    public interface IUser
+    {
+        IList<UserDto> GetAll();
+        UserDto GetById(int id);
+        UserDto Create(UserDto person);
+        void Update(int id, UserDto person);
+        UserDto Delete(int id);
+    }
+}
---
+++ ListSmarter/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using ListSmarter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListSmarter.Services
+{
+    public interface IUserService
+    {
+        IList<UserDto> GetUsers();
+        UserDto GetUser(int id);
+        UserDto CreateUser(UserDto person);
+        void UpdateUser(int personId, UserDto person);
+        UserDto DeleteUser(int personId);
+    }
+}
